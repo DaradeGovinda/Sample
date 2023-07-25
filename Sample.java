9e45d523d26103000e511613c5f47853edfb8887
Govinda Darade <daradegovinda7@gmail.com>
2023-07-25 09:17:53 +0530

    This is first commit

---
+++ Sample.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Sample{
+public static void main(String args[]) {
+    int x,y,z;
+    System.out.println("Enter Value for x , y and z::");    
+    Scanner sc=new Scanner(System.in);
+    x=sc.nextInt();
+    y=sc.nextInt();
+    z=sc.nextInt();
+    System.out.println("Maximum of "+x+","+y+" and "+z+" ::"+((x>y && x>z)?x:((y>z)?y:z)));
+}
+}
